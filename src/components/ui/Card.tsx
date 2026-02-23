386f43c12e98f31a5c9523f5faabf405106301e0
Francisco Mesa <francisco.mc.ids@gmail.com>
2026-02-22 21:15:33 -0400

    refactor: Adjust Card component's className application order for proper merging

--- src/components/ui/Card.tsx
+++ src/components/ui/Card.tsx
@@ -47,12 +47,12 @@ export function Card({
       <div
         className={cn(
           "bg-white dark:bg-gray-900 p-4 rounded-xs border border-gray-200 dark:border-gray-700 ring-1 ring-gray-500/5 dark:ring-gray-500/10",
-          className,
           {
             "fixed z-50 m-auto overflow-auto inset-0": isExpanded,
             "sm:inset-4 sm:max-h-[calc(100vh-4rem)] sm:max-w-[calc(100vw-4rem)]": isExpanded,
             "lg:inset-12 lg:max-h-[calc(100vh-6rem)] lg:max-w-[calc(100vw-12rem)]": isExpanded,
           },
+          className,
         )}
         style={{
           position: isExpanded ? "fixed" : "relative",
